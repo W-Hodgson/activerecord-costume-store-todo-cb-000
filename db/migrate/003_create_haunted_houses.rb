a6079d94704462d0646dcd606f7282d22b3afc7d
Will Hodgson <will.hodgson@current-rms.com>
2019-10-10 08:53:17 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,7 +6,7 @@ class CreateHauntedHouses < ActiveRecord::Migration
       t.string :name,
       t.string :location,
       t.string :theme,
-      t.float :price, 
+      t.float :price,
       t.boolean :family_friendly,
       t.datetime :opening_date,
       t.datetime :closing_date,
