72a911c8d4d86fa0ebde1b5b9795488314b5a439
Will Hodgson <will.hodgson@current-rms.com>
2019-10-10 08:44:27 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,7 +14,8 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
       t.string :name,
       t.integer :price,
       t.string :size,
-      t.string :image_url
+      t.string :image_url,
+      t.timestamps
     end
   end
 end
